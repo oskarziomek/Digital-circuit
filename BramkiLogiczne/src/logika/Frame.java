ab7650939d42321bcfced8083031d2765c9d8b25
Admin <Admin@AdminDesktop.home>
2016-09-23 08:13:20 +0200

    Aktualizacja interfejsu, dzialający import pliku i wydruk tablicy

--- BramkiLogiczne/src/logika/Frame.java
+++ BramkiLogiczne/src/logika/Frame.java
@@ -9,7 +9,6 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -25,16 +24,15 @@ import java.util.Stack;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.Color;
-import javax.swing.JLabel;
 import java.awt.SystemColor;
+import javax.swing.JTextPane;
+import java.awt.Rectangle;
 
 public class Frame extends JFrame {
 
 	private JPanel contentPane;
-	private JTable table;
 	private String infix = "";
-	JLabel lblNewLabel = new JLabel(" ");
-	
+		
     
 	public static void main(String[] args) {
 		
@@ -96,23 +94,41 @@ public class Frame extends JFrame {
 	}
 
 	public Frame() {
-		setTitle("Bramki Logiczne");
+		setTitle("Bramki Logiczne - wybierz plik");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 673, 528);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(112, 128, 144));
+		panel.setBackground(new Color(0, 0, 128));
 		contentPane.add(panel, BorderLayout.NORTH);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.DARK_GRAY);
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel_1.add(scrollPane);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(new Rectangle(10, 10, 10, 10));
+		scrollPane.setViewportView(textPane);
+		textPane.setFont(new Font("Courier New", Font.BOLD, 15));
+		textPane.setForeground(SystemColor.window);
+		textPane.setCaretColor(Color.DARK_GRAY);
+		textPane.setBackground(Color.DARK_GRAY);
+		
 		JButton btnWczytajPlikI = new JButton("Wczytaj plik i poka¿ tablicê prawdy");
+		btnWczytajPlikI.setFocusable(false);
 		btnWczytajPlikI.setToolTipText("Wczytuje dzia\u0142anie boolowskie z zewn\u0119trznego pliku, a nast\u0119pnie oblicza jego warto\u015B\u0107 dla wszystkich kombinacji zmiennych");
 		btnWczytajPlikI.setBackground(new Color(0, 0, 0));
 		btnWczytajPlikI.setForeground(new Color(0, 0, 0));
 		btnWczytajPlikI.setFont(new Font("Tahoma", Font.BOLD, 11));
+		
 		btnWczytajPlikI.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				infix = czytajWyrazenieZPliku(czytajPlik());
@@ -123,28 +139,35 @@ public class Frame extends JFrame {
 		        
 		        Wyliczalne a = pw.parsuj(infixNaPrefix(infix));
 		        
-		        wypiszPrawdy(a, new ArrayList<>(pw.zwrocZmienne().values()));
+		        int liczbaZmiennych = sprawdzLiczbeZmiennych(infixNaPrefix(infix));
+		        String zmienne = sprawdzZmienne(new ArrayList<>(pw.zwrocZmienne().values()));
 		        
-		        lblNewLabel.setText(infix);
+		        System.out.println(liczbaZmiennych);
+		        
+		        String wynik = wypiszPrawdy(a, new ArrayList<>(pw.zwrocZmienne().values()));		        
+		        String temp = wynik.replaceAll("false", "0");
+		        String result = temp.replaceAll("true", "1");
+		        
+		        textPane.setEditable(false); 
+		        textPane.setText("Wczytano wyra¿enie: " + infix + "\n\n" + zmienne + "    Wynik:" + "\n" + result);
+		        
 		        
 			}
 		});
+		
 		panel.add(btnWczytajPlikI);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(SystemColor.desktop);
-		contentPane.add(panel_1, BorderLayout.CENTER);
-		lblNewLabel.setFont(new Font("Courier New", Font.BOLD, 13));
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBackground(Color.WHITE);
 		
-		panel_1.add(lblNewLabel);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		panel_1.add(scrollPane);
-		
-		table = new JTable();
-		scrollPane.setViewportView(table);
+	}
+	
+	public int sprawdzLiczbeZmiennych (String prefix) {
+		int liczba = 0;
+		for (int i = 0; i < prefix.length(); i++) {
+			if (Character.isLetter(prefix.charAt(i))) {
+				liczba++;
+			}
+		}
+		return liczba;
 	}
 	
 	public String czytajPlik() {
@@ -155,7 +178,6 @@ public class Frame extends JFrame {
 		int result = fileChooser.showOpenDialog(this);
 		if (result == JFileChooser.APPROVE_OPTION) {
 			selectedFile = fileChooser.getSelectedFile();
-		    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
 		}
 		wynik = selectedFile.getAbsolutePath();	
 		return wynik;	
@@ -177,15 +199,29 @@ public class Frame extends JFrame {
         }
         return kombinacje;
     }
+	
+	private static String sprawdzZmienne(ArrayList<Zmienna> zmienne) {
+    	String tmp = "[";
+        for (int i = 0; i < zmienne.size(); i++) {
+        	 tmp += zmienne.get(i).getSymbol();
+        	 tmp += ", ";
+        }
+        String wynik = tmp.substring(0, tmp.length()-2);
+        wynik += "]";
+        return wynik;
+	}
 
-    private static void wypiszPrawdy(Wyliczalne korzen, ArrayList<Zmienna> zmienne) {
+    private static String wypiszPrawdy(Wyliczalne korzen, ArrayList<Zmienna> zmienne) {
+    	String wynik = "";
         ArrayList<boolean[]> kombinacje = kombinatron(zmienne.size());
+        
         for (boolean[] kombinacja : kombinacje) {
             for (int i = 0; i < kombinacja.length; i++) {
                 zmienne.get(i).setWartoscLogiczna(kombinacja[i]);
             }
-            System.out.println(Arrays.toString(kombinacja) + " " + korzen.Wylicz());
+            wynik += Arrays.toString(kombinacja) + " ->   " + korzen.Wylicz() + "\n";
         }
+        return wynik;
     }
 
     private static String infixNaPrefix(String infix) {
